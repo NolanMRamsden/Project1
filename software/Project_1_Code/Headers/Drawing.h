5d2c0fdd1162c6c721d760333412ede1fc04f9aa
WilliamGeng <will-gw@hotmail.com>
2014-10-09 17:00:15 -0700

    Added a gun

--- software/Project_1_Code/Headers/Drawing.h
+++ software/Project_1_Code/Headers/Drawing.h
@@ -19,7 +19,7 @@ extern void clearCharacters();
 extern void drawBall(Ball *ball);
 extern void coverBall(int x, int y, int colour);
 extern void drawPaddle(Paddle *paddle);
-extern void coverPaddle(int x, int y, int width, int colour);
+extern void coverPaddle(int x, int y, int width, int colour, int gunMounted);
 extern void drawBrick(Brick *brick);
 extern void coverBrick(int x, int y, int health);
 extern void drawText(char *text, int x, int y, int selected);
